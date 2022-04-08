48acc32112ff23bedad34e05044e75385f6b1049
mini0-0 <nalala8200@naver.com>
2022-04-08 16:13:30 +0900

    post에 category 필드 추가

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -4,8 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=20, unique=True, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -20,7 +27,7 @@ class Post(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User,null=True ,on_delete=models.SET_NULL)
-
+    category = models.ForeignKey(Category, null=True, blank=True , on_delete=models.SET_NULL)
 
 
     def __str__(self):
