e7624902e3b1f089ba4f8b38265c290070165862
mini0-0 <nalala8200@naver.com>
2022-04-08 17:22:57 +0900

    카테고리 뷰 생성, 테그 모델 추가

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -4,12 +4,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=20, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=20, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -26,9 +39,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User,null=True ,on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True , on_delete=models.SET_NULL)
-
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
