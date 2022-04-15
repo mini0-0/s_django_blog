a10473a75e80c99c7318f3f117d992250a9eec60
mini0-0 <nalala8200@naver.com>
2022-04-15 17:34:34 +0900

    post create,post update ,login check

--- blog_main/blog/views.py
+++ blog_main/blog/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post,Category
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Category, Tag
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user
+        if current_user.is_authenticated and current_user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 class PostList(ListView):
     model = Post
@@ -32,6 +61,18 @@ def categories_page(request, slug):
         'categories' : Category.objects.all(),
         'no_category_post_count' : Post.objects.filter(category=None).count(),
         'category' : category,
+        'post_list': post_list
+    }
+    return render(request, 'blog/post_list.html', context)
+
+def tag_page(request, slug):
+
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    context = {
+        'categories' : Category.objects.all(),
+        'no_category_post_count' : Post.objects.filter(category=None).count(),
+        'tag': tag,
         'post_list': post_list
     }
     return render(request, 'blog/post_list.html', context)
