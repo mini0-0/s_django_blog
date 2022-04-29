f9e4e450057d22a4c2e0a4c489647d115b5985d5
mini0-0 <nalala8200@naver.com>
2022-04-29 16:19:45 +0900

    markdown, crispy-form

--- blog_main/blog_main/settings.py
+++ blog_main/blog_main/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdownx',
+    'crispy_forms',
     'blog',
     'single_pages',
+
 ]
 
 MIDDLEWARE = [
@@ -121,7 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
